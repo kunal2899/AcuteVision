8bfb5934b339e3d969e39e10b9111e06963fe7b5
Kunal Jain <kunaljain649@gmail.com>
2020-12-13 22:47:13 +0530

    dashboard added

--- av-web/src/app/app-routing.module.ts
+++ av-web/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogRegComponent } from './log-reg/log-reg.component';
 
 const routes: Routes = [
-  { path: '' , pathMatch: 'full' , redirectTo: '/log-reg' },
-  { path: 'log-reg' , component: LogRegComponent }
+  { path: '' , pathMatch: 'full' , redirectTo: '/dashboard' },
+  { path: 'log-reg' , component: LogRegComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- av-web/src/app/app.module.ts
+++ av-web/src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { LogRegComponent } from './log-reg/log-reg.component';
 import {FormsModule} from '@angular/forms'
 import { ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogRegComponent
+    LogRegComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
